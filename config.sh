68bde8618dfae1fe97da228fb357de1c666e54a5
Chrysoula Stathakopoulou <stchrysa@gmail.com>
2017-10-09 17:56:39 +0200

    Updated the configuration script for multiple orderers.

--- config.sh
+++ config.sh
@@ -1,5 +1,7 @@
 ordererType="solo" 	# solo or kafka
 orderer="vm0"		
-peers="vm1 vm2"
-ensamble="vm3" 		# Leave string empty for solo
-brokers="vm4 vm5"  	# Leave string empty for solo
+orgs="Org1 Org2"
+peers="vm1 vm2 vm3"
+declare -A peerOrgs=( ["Org1"]="vm1" ["Org2"]="vm2 vm3" )
+ensamble="vm4" 		# Zookeeper Ensemble: Leave string empty for solo
+brokers="vm5 vm6"  	# Kafka Brokers: Leave string empty for solo
